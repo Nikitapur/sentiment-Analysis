579face9ab6bbbae187df204638b9d80f80db95b
m-triple-m <triplem656@gmail.com>
2021-08-05 18:44:58 +0530

    ..

--- App.py
+++ App.py
@@ -6,11 +6,17 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import re
 import string
-from wordcloud   import WordCloud, STOPWORDS
+from wordcloud import WordCloud, STOPWORDS
+from textblob import TextBlob
+
+punc = string.punctuation
+stop_words = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
 
 
 st.title("SENTIMENT ANALYSIS")
-sidebar = st.sidebar  
+sidebar = st.sidebar
 st.image("abc.jpg")
 st.text("Sentiment analysis refers to identifying as well as classifying the sentiments that are expressed in the text source. Tweets are often useful in generating a vast amount of sentiment data upon analysis.")
 sidebar = st.sidebar
@@ -20,17 +26,19 @@ choice = sidebar.selectbox("Select Option", choice_list)
 
 df = pd.read_csv("dataset/covid19_tweets.csv").head(1000)
 
+
 def View_DataSet():
     st.markdown("""
        ## NAME
        **Anurag Pathak**
     """)
 
+
+@st.cache()
 def clean_text(text):
-    
     '''Make text lowercase, remove text in square brackets,remove links,remove punctuation
     and remove words containing numbers.'''
-    
+
     text = text.lower()
     text = re.sub('\[.*?\]', '', text)
     text = re.sub('https?://\S+|www\.\S+', '', text)
@@ -38,45 +46,69 @@ def clean_text(text):
     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
     text = re.sub('\n', '', text)
     text = re.sub('\w*\d\w*', '', text)
-    
-    #Removing stopwords
-    text=" ".join([word for word in str(text).split() if word not in stop_words])
-    
-    #Stemming
+
+    # Removing stopwords
+    text = " ".join([word for word in str(
+        text).split() if word not in stop_words])
+
+    # Stemming
     text = " ".join([stemmer.stem(word) for word in text.split()])
-    
-    #Lemmatization
+
+    # Lemmatization
     text = " ".join([lemmatizer.lemmatize(word) for word in text.split()])
-    
+
     return text
 
+
+@st.cache()
 def remove_emoji(text):
     emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           u"\U00002702-\U000027B0"
-                           u"\U000024C2-\U0001F251"
-                           "]+", flags=re.UNICODE)
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', text)
-
 
 
 def analyseSentiment():
     st.header('detail')
+
+    df['text'] = df['text'].apply(lambda x: remove_emoji(x))
+
+    df['text'] = df['text'].apply(lambda x: clean_text(x))
+    sentiments = []
+    subjectivity = []
+    for tweet in df['text'].values:
+        blob = TextBlob(tweet)
+        senti = blob.sentiment.polarity
+        if senti > 0:
+            sentiments.append('positive')
+        elif senti < 0:
+            sentiments.append('negative')
+        else:
+            sentiments.append('neutral')
+        subjectivity.append(blob.sentiment.subjectivity)
+    
+    df['sentiment'] = sentiments
+    df['subjectivity'] = subjectivity
+
+
+
+    data= df.groupby('sentiment').count().reset_index()
+    st.dataframe(df)
+    st.plotly_chart(plotPie(data=df, values='text', labels='sentiment', title='title'))
+
 
 def analyseEDA():
     st.header('Exploratoy Data Analysis')
 
     st.dataframe(df)
 
-    st.plotly_chart(plotBar(df.groupby('user_location').count().reset_index().sort_values('user_location', ascending=False).head(10), 'user_location','date','title' ))
-
-
-    df['text'] = df['text'].apply(lambda x: remove_emoji(x))
-
-    df['text'] = df['text'].apply(lambda x: clean_text(x))
+    st.plotly_chart(plotBar(df.groupby('user_location').count().reset_index().sort_values(
+        'user_location', ascending=False).head(10), 'user_location', 'date', 'title'))
 
 
 if choice == choice_list[0]:
@@ -85,7 +117,3 @@ elif choice == choice_list[1]:
     analyseSentiment()
 elif choice == choice_list[2]:
     analyseEDA()
-
-
-
-
--- visualize.py
+++ visualize.py
@@ -5,3 +5,5 @@ def plotBar(data, x, y, title):
     return px.bar(data, x = x, y = y, title = title)
 
     
+def plotPie(data, labels, values, title):
+    return px.pie(data, labels = labels, values = values, title = title)
